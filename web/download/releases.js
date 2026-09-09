b2bbf6692abd61f407addb83042341bcf54e5c99
wisdomzo <zhaoou0427@icloud.com>
2026-09-09 14:15:51 +0900

    v2.8.2, 【发布页】追加了Intel下载链接

--- web/download/releases.js
+++ web/download/releases.js
@@ -3,6 +3,6 @@
    Optional version example: 'v2.7.4'. No network request is made here. */
 window.ASSET_RELEASES = {
   macArm: { url: 'https://pub-564c5917b4c6455d8c1b056119bd1579.r2.dev/RSS_Predictor_ARM_v2.8.2.dmg', version: 'v2.8.2' },
-  macIntel: { url: null, version: null },
+  macIntel: { url: 'https://pub-564c5917b4c6455d8c1b056119bd1579.r2.dev/RSS_Predictor_Intel_v2.8.2.dmg', version: 'v2.8.2' },
   windows: { url: 'https://pub-564c5917b4c6455d8c1b056119bd1579.r2.dev/RSS_Predictor_windows_v2.8.2.zip', version: 'v2.8.2' }
 };
